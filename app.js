e57f7f99fc3923b17181ea5ad6885f60779053f5
gothica <semidevilish666@gmail.com>
2022-11-26 21:09:34 +0300

    fix: deploy

--- app.js
+++ app.js
@@ -2,22 +2,18 @@ const express = require("express");
 const path = require("path");
 const config = require("config");
 const mongoose = require("mongoose");
-
+const PORT = process.env.port || 5000;
 const app = express();
 
 app.use(express.json({ extended: true }));
 app.use("/api/auth", require("./routes/auth.routes"));
 app.use("/api/users", require("./routes/data.routes"));
 
-if (process.env.NODE_ENV === "production") {
-  app.use("/", express.static(path.join(__dirname, "client", "build")));
+app.use("/", express.static(path.join(__dirname, "./client/build")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
-
-const PORT = config.get("port") || 5000;
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./client/build/index.html"));
+});
 
 async function start() {
   try {
