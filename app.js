e8efa68d5a6ac73d9d7645c7db458e0628badd04
gothica <semidevilish666@gmail.com>
2022-11-26 19:20:22 +0300

    feat: to deploy

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const config = require("config");
 const mongoose = require("mongoose");
 
@@ -7,6 +8,15 @@ const app = express();
 app.use(express.json({ extended: true }));
 app.use("/api/auth", require("./routes/auth.routes"));
 app.use("/api/users", require("./routes/data.routes"));
+
+if (process.env.NODE_ENV === "production") {
+  app.use("/", express.static(path.join(__dirname, "client", "build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const PORT = config.get("port") || 5000;
 
 async function start() {
